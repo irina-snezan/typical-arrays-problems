b8c14a45ed4156edb41b82766e75676393d5ca77
Irina Shabuldajeva <70856550+irina-snezan@users.noreply.github.com>
2020-10-02 19:45:22 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined) {
+	  return 0;
+	} else if (array.length === 0) {
+		return 0;
+	} else {
+	let min = array[0];
+	for(let i=1; i<array.length; i++){
+		if(array[i]<min) {min = array[i];}
+	}
+	return min;
+	}  
 }
 
 exports.max = function max (array) {
-  return 0;
+	 if(array != undefined && array.length != 0) {
+	let max = array[0];
+	for(let i=1; i<array.length; i++){
+		if(array[i]>max) {max = array[i];}
+	}
+	return max;
+	} else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if(array === undefined) {
+	  return 0;
+	} else if (array.length === 0) {
+		return 0;
+	} else {
+		let avg = 0;
+		for(let i=0; i<array.length; i++){
+			avg += array[i];
+		}
+		avg = avg / array.length;
+	return avg;
+	}  
+  
 }
